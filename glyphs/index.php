5198db208c40ba8a9beb7de3742a632388ce4b28
Wych <wychwitchcraft@gmail.com>
2022-04-24 23:51:56 -0500

    update

--- glyphs/index.php
+++ glyphs/index.php
@@ -3,14 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/web.css" rel="stylesheet">
-        
-        <meta property="og:url" content="https://atelierwy.ch/" />
-        <meta property="og:type" content="homepage" />
-        <meta property="og:title" content="Atelier Wych" />
-        <meta property="og:description" content="Bididi bopidi boo" />
-        <meta property="og:image" content="https://atelierwy.ch/img/screenshot.png" />
-        <meta property="twitter:card" content="summary_large_image">
+        <link href="../css/web.css" rel="stylesheet">
         <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
     </head>
     <body>
@@ -20,8 +13,8 @@
                 <label for="menuButton"><span class="iconify" data-icon="fluent:text-grammar-wand-24-regular" style="color: #4e3d57;" data-flip="horizontal" data-width="60"></span></label>
                 <input type="checkbox" id="menuButton">
                 <ul>
-                    <li><a href="index.php">home</a></li>
-                    <li><a href="glyphs/index.php">glyphs</a></li>
+                    <li><a href="../index.php">home</a></li>
+                    <li><a href="../glyphs/index.php">glyphs</a></li>
                     <li><a href="#" class="disabled">updates</a class="disabled"></li>
                     <li><a href="#" class="disabled">shrines</a></li>
                     <li><a href="#" class="disabled">thoughts</a></li>
@@ -32,16 +25,16 @@
                 <a href="index.php">
             <?php
                 $images = [
-                    0 => "<img src=\"img/tarot/fool.svg\">",
-                    1 => "<img src=\"img/tarot/magician.svg\">",
-                    2 => "<img src=\"img/tarot/star.svg\">",
-                    3 => "<img src=\"img/tarot/empress.svg\">",
-                    4 => "<img src=\"img/tarot/hangedman.svg\">",
-                    5 => "<img src=\"img/tarot/moon.svg\">",
-                    6 => "<img src=\"img/tarot/hermit.svg\">",
-                    7 => "<img src=\"img/tarot/wheel.svg\">",
-                    8 => "<img src=\"img/tarot/world.svg\">",
-                    9 => "<img src=\"img/tarot/tower.svg\">"
+                    0 => "<img src=\"../img/tarot/fool.svg\">",
+                    1 => "<img src=\"../img/tarot/magician.svg\">",
+                    2 => "<img src=\"../img/tarot/star.svg\">",
+                    3 => "<img src=\"../img/tarot/empress.svg\">",
+                    4 => "<img src=\"../img/tarot/hangedman.svg\">",
+                    5 => "<img src=\"../img/tarot/moon.svg\">",
+                    6 => "<img src=\"../img/tarot/hermit.svg\">",
+                    7 => "<img src=\"../img/tarot/wheel.svg\">",
+                    8 => "<img src=\"../img/tarot/world.svg\">",
+                    9 => "<img src=\"../img/tarot/tower.svg\">"
                 ];
                 $keywords = [
                     0 => "<p class=\"keyword\">Beginnings, innocence, spontaneity, a free spirit</p>",
@@ -61,18 +54,29 @@
                 echo $images[$i];
             ?>
             </a>
-            
-                
                     <h1>Atelier Wych</h1>
                     <p>welcome to my space.</p>
             </section>
-        </header>
 
+            <section class="title">
+                    <h2>Glyphs Page</h2>
+                    <p>This is a page for collecting and sharing the glyphs I have created! I don't have a system rn, but I would like to create one soon.</p>
+            </section>
+        </header>
+                
         <main>
             <section>
-
+                <h3>Inspiration</h3>
+                <img src="img/glyphs/Inspiration.svg">
                 <p>
-                    Hello! I'm wych(witch) but you can just call me wych! I'm a poc transgirl who likes witches, coding, and nerd shit lmao. This is a space for me to shout to the void that is the internet!! I have some plans for some shrines, articles to write, and more! Right now I just have my Glyphs that you can look at ^^ enjoy your stay!
+                    This is my glyph for creative inspiration! Like a lighthouse calling ships to port, this calls creativity to you.
+                </p>
+            </section>
+            <section>
+                <h3>Study</h3>
+                <img src="img/glyphs/Study.svg">
+                <p>
+                    This is my Glyph for studying and learning! I would draw tons of these on the margins of my notes for good luck and focus, and deeper understanding of the material. Also for friends when they need good vibes for a test!
                 </p>
             </section>
         </main>
